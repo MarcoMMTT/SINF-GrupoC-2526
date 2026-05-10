da16d0f10f9ea28f1c5b15adf38a3e6fa4af89fb
marcos <marcos.sanjurjo.besada@gmail.com>
2026-05-10 19:43:34 +0200

    Algo funciona

---
+++ insertar_datos/insertar_todo.sql
@@ -0,0 +1,19 @@
+USE GrupoC;
+-- =========================================
+-- FICHERO MAESTRO DE CARGA
+-- =========================================
+
+SOURCE numeros_aux.sql;
+SOURCE insertar_usuarios.sql;
+SOURCE isertar_espectaculos.sql;
+SOURCE recintos_aux.sql;
+SOURCE clientes.sql;
+SOURCE insertar_evento.sql;
+SOURCE insertar_gradas_y_aforo.sql;
+SOURCE oferta.sql;
+SOURCE entradas.sql;
+SOURCE ventas.sql;
+
+-- =========================================
+-- FIN DE CARGA
+-- =========================================
